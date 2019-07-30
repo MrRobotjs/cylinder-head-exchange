a59a5a6502b76bf2245a33bea4e97ba4c79a966c
Christopher <christopher.ramos.00@outlook.com>
2019-07-30 07:59:36 -0700

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,12 +32,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Cylinder Head Exchange`,
+        short_name: `CHX`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
+        background_color: `#ffffff`,
+        theme_color: `#ff2b3c`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
