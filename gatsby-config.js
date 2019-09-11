66b915e2dcb98f6ef8c6712d9b44663004afdc8f
Christopher <christopher.ramos.00@outlook.com>
2019-09-11 09:11:37 -0700

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,7 +54,7 @@ module.exports = {
         background_color: `#ff2b3c`,
         theme_color: `#ff2b3c`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Favicon512.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
