a9c7720e4f100a63c1d99b646a314652692ec06a
Christopher <christopher.ramos.00@outlook.com>
2019-09-16 20:45:17 -0700

    it worked (ink hidden until tapped on mobile)

--- src/components/offer-card-2.js
+++ src/components/offer-card-2.js
@@ -61,6 +61,7 @@ const Card = styled.div`
       box-shadow: 2px 2px 15px 0 rgba(60,60,60,.2);
       -webkit-user-select: none;
       -webkit-touch-callout: none;
+      background-color: #eaeaea;
       ${Url} {
         visibility: visible;
       }
