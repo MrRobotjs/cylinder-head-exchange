cd78718b50385447f4ffd0afa6f9153f81615f07
Christopher <christopher.ramos.00@outlook.com>
2019-09-18 12:52:07 -0700

    preconnect font/map api

--- src/components/helmet.js
+++ src/components/helmet.js
@@ -38,6 +38,10 @@ const Helmett = ({ title, description }) => (
           <meta itemProp="name" content={title} />
           <meta itemProp="description" content={metaDescription} />
 
+          <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+          <link rel="preconnect" href="https://maps.googleapis.com/" crossorigin />
+          <link rel="preconnect" href="https://fonts.googleapis.com/" crossorigin />
+          <link rel="preconnect" href="https://maps.gstatic.com/" crossorigin />
           <link rel="mask-icon" href={Safari} color={variable.SiteColor}/>
 
           <meta name="msapplication-TileColor" content="#2b5797"/>
