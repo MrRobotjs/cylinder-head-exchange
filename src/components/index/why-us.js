5f92dbda96749da7b36d495861206c4b28b81264
Christopher <christopher.ramos.00@outlook.com>
2019-10-14 07:56:25 -0700

    Update

--- src/components/index/why-us.js
+++ src/components/index/why-us.js
@@ -454,7 +454,7 @@ padding: 5rem 0;
         ${Why} {
             margin-bottom: 1.5rem;
             line-height: 1.9;
-            color: #424242;
+            color: #828282;
         }
         ul {
             margin: unset;
